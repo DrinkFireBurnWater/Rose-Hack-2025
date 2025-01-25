3eaccc6c6c1dc5d1521703975bd677273f603e48
William Wong <120128836+wwong0@users.noreply.github.com>
2025-01-25 14:45:00 -0800

    flask

--- routes.py
+++ routes.py
@@ -1,18 +1,15 @@
-from flask import current_app as app
-from flask import render_template, request, jsonify, make_response
+from flask import Flask, render_template, request, jsonify
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/test", methods = ['POST'])
+@app.route('/test', methods=['POST'])
 def test():
-    test_dict = {'test': 'test'}
-    test_response = make_response(jsonify(test_dict))
-    test_response.status_code = 200
-    return test_response
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
